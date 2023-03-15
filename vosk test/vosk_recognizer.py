7c0b43a52862191c699fb3320a4215fb5382e6c6
Lamoshin <r.vlad20031503@gmail.com>
2023-03-15 18:36:25 +1000

    Updated input and work cycle

--- vosk test/vosk_recognizer.py
+++ vosk test/vosk_recognizer.py
@@ -6,33 +6,47 @@ os.chdir("vosk test")
 
 FN = "file.wav"
 
-try:
-    while True:
-        input("Нажмите Enter для записи и мгновенного распознавания. Нажмите Ctrl-C для выхода.")
+while True:
+    try:
+        while True:
+            input(
+                "Нажмите Enter для записи и мгновенного распознавания. Нажмите Ctrl-C для выхода.")
 
+            try:
+                print("Запись!")
+                result = vosk_class.STT.decode()
+                print("Завершение записи.")
+                with open("data.json", "w", encoding="UTF-8") as F:
+                    json.dump(result, F, ensure_ascii=False, indent=4)
+
+                print("Расшифровано слово: {0}".format(result["text"]))
+
+            except FileNotFoundError:
+                print("\nОшибка! Файл еще не создан. \nСоздайте файл при помощи другого скрипта и повторите попытку. \nЭто приложение можно не закрывать!\n")
+
+            except Exception as ex:
+                print("Необработанное исключение.")
+                print("Имя исключения: {0}".format(type(ex).__name__))
+                print("Аргументы: {0}".format(ex.args))
+                input_string = ""
+                while (input_string not in ("0", "1")):
+                    input_string = input(
+                        "Введите 1, чтобы проигнорировать исключение, или 0, чтобы выйти из программы: ")
+                if (input_string == "0"):
+                    raise
+
+    except KeyboardInterrupt:
         try:
-            print("Запись!")
-            result = vosk_class.STT.decode()
-            print("Завершение записи.")
-            with open("data.json", "w", encoding="UTF-8") as F:
-                json.dump(result, F, ensure_ascii=False, indent=4)
-
-            print("Расшифровано слово: {0}".format(result["text"]))
-
-        except FileNotFoundError:
-            print("\nОшибка! Файл еще не создан. \nСоздайте файл при помощи другого скрипта и повторите попытку. \nЭто приложение можно не закрывать!\n")
-
-        except Exception as ex:
-            print("Необработанное исключение.")
-            print("Имя исключения: {0}".format(type(ex).__name__))
-            print("Аргументы: {0}".format(ex.args))
-            input_string = ""
-            while (input_string not in ("0", "1")):
-                input_string = input(
-                    "Введите 1, чтобы проигнорировать исключение, или 0, чтобы выйти из программы: ")
-            if (input_string == "0"):
-                raise
-
-except KeyboardInterrupt:
-    print("\nВыход из программы...")
-    exit()
+            while True:
+                n = input(
+                    "\nВы хотите выйти из программы или перезапустить модель? (1 = выйти из программы, 2 = перезапустить) ")
+                if n == "1":
+                    print("Выход из программы...")
+                    exit()
+                elif n == "2":
+                    break
+                else:
+                    print("Некорректный ввод. Повторите попытку.")
+        except KeyboardInterrupt:
+            print("\nВыход из программы...")
+            exit()
